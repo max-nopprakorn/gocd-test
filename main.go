171d868460df4b996170547a5724b965a9619eb3
Max <nopprakorn.sarinsakul@gmail.com>
2019-05-15 12:09:16 +0700

    REMOVE LOOP

--- main.go
+++ main.go
@@ -6,10 +6,8 @@ import (
 )
 
 func main() {
-	for {
-		fmt.Println("HELLO GO CD")
-		fmt.Println("WORKWORKWOKR")
-		fmt.Println()
-		time.Sleep(2 * time.Second)
-	}
+	fmt.Println("HELLO GO CD")
+	fmt.Println("WORKWORKWOKR")
+	fmt.Println()
+	time.Sleep(2 * time.Second)
 }
